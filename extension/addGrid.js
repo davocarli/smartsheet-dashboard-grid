02fee3604ae2c3b173d68d0500f6fd177a39b116
David Carli-Arnold <davocarli@gmail.com>
2022-02-07 10:53:37 -0800

    Update grid system

--- extension/addGrid.js
+++ extension/addGrid.js
@@ -1,94 +1,20 @@
 gridLayouts = {
     "12Columns":    [
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Blue', 'opacity': '10%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'Red', 'opacity': '20%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
-        {'color': 'White', 'opacity': '0%', 'height': '100%'},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 3, 'y':0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 10, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 17, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 24, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 31, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 38, 'y': 0},
+
+        {'color': 'Blue', 'opacity': '20%', 'height': -1, 'width': 1, 'x': 44, 'y': 0},
+        
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 45, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 52, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 59, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 66, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 73, 'y': 0},
+        {'color': 'Red', 'opacity': '20%', 'height': -1, 'width': 6, 'x': 80, 'y': 0},
     ]
 }
 
@@ -108,14 +34,34 @@ function toggleHighlight(imgUrl) {
     } else {
         chosenLayout = gridLayouts['12Columns'];
         parent = document.querySelectorAll("[data-client-id='dab-dui-2']")[0];
-        gridWidth = parent.offsetWidth / chosenLayout.length;
+        // gridWidth = parent.offsetWidth / chosenLayout.length;
+        gridDimensions = parent.style.backgroundSize.replaceAll('px', '').split(' ');
+        x = parseInt(gridDimensions[0]);
+        y = parseInt(gridDimensions[1]);
+
+        newHTML = '<div style="position: relative; overflow: hidden; height: 100%; width: 100;" data-grid-overlay="parent">'
 
         for (i = 0; i < chosenLayout.length; i++) {
             column = chosenLayout[i];
+            left = (x * column['x']) + 'px';
+            top = (y * column['y']) + 'px';
+            height = null;
+            width = null;
+            if (column['height'] == -1) {
+                height = '100%';
+            } else {
+                height = (y * column['height']) + 'px';
+            }
+            if (column['width'] == -1) {
+                width = '100%';
+            } else {
+                width = (x * column['width']) + 'px';
+            }
             // if (column['filled']) {
-            gridHighlightElement = "<div data-grid-overlay=\"" + i + "\" style=\"height: " + column['height'] + "; width: " + gridWidth + "px;  background-color: " + column['color'] + "; opacity: " + column['opacity'] + "; display: inline-block;\"></div>"
+            gridHighlightElement = "<div data-grid-overlay=\"" + i + "\" style=\"height: " + height + "; width: " + width + ";  background-color: " + column['color'] + "; opacity: " + column['opacity'] + "; position: absolute; left: " + left + "; top: " + top + "; overflow: hidden;\"></div>"
                 // gridHighlightElement = "<div data-grid-overlay=\"" + column['columnNumber'] + "\" style=\"width: " + gridWidth + "%; background-color: " + column['color'] + "; opacity: " + column['opacity'] + "; margin-left: " + (gridWidth * column['columnNumber']) + "%; pointer-events: none; z-index: 5000;\" class=\"dashboard-helper-grid dashboard-helper-grid--active\"></div>"
-            parent.innerHTML += gridHighlightElement;
+            newHTML += gridHighlightElement;
+                // parent.innerHTML += gridHighlightElement;
                 // parent.insertAdjacentHTML('afterend', gridHighlightElement);
             // }
             // if (chosenLayout[i] == 1) {
@@ -125,6 +71,8 @@ function toggleHighlight(imgUrl) {
             //     console.log('no grid');
             // }
         }
+
+        parent.innerHTML = newHTML + "</div>";
         
         // gridHighlightElement = "<div id=\"grid-highlight\" class=\"dashboard-helper-grid dashboard-helper-grid--active\" style=\"background-size: 100%; background-image:url('" + imgUrl + "'); opacity: 30%; pointer-events: none; z-index: 5000;\"></div>"
 
